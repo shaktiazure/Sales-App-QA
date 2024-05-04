aba57fb83758a53900bc58768ec503dc36b88915
shakti kumar <shakti2008kumar@gmail.com>
2024-05-04 09:32:18 +0400

    final

--- Terraform/Resourcegroup/main.tf
+++ Terraform/Resourcegroup/main.tf
@@ -1,1 +1,87 @@
-Hello
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.102.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+  }
+}
+
+resource "azurerm_resource_group" "QA_RG" {
+  name     = "Sales-QA-RG"
+  location = "West Europe"
+}
+
+variable "prefix" {
+  default = "salesqa"
+}
+
+
+resource "azurerm_virtual_network" "qa-vnet" {
+  name                = "${var.prefix}-network"
+  address_space       = ["20.0.0.0/16"]
+  location            = azurerm_resource_group.QA_RG.location
+  resource_group_name = azurerm_resource_group.QA_RG.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.QA_RG.name
+  virtual_network_name = azurerm_virtual_network.qa-vnet.name
+  address_prefixes     = ["20.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "qa-nic" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.QA_RG.location
+  resource_group_name = azurerm_resource_group.QA_RG.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "QA-VM" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.QA_RG.location
+  resource_group_name   = azurerm_resource_group.QA_RG.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
